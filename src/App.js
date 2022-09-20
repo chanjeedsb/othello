90c312f44c7c9b46dad2841fb6ad2dac806578db
Chanya Kukulprasong <chanyakukulprasong.sb@gmail.com>
2022-09-21 01:41:03 +0700

    no endgame judgement

--- src/App.js
+++ src/App.js
@@ -1,41 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
-const App2 = () => {
-  const [board, setBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, -1, 1, 0, 0, 0],
-    [0, 0, 0, 1, -1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
-  const [currentState, setCurrentState] = useState(1);
-
-  const handleClick = (x, y) => {
-    const deepCopy = JSON.parse(JSON.stringify(board));
-    deepCopy[x][y] = currentState;
-    setBoard(deepCopy);
-  };
-
-  return (
-    <>
-      {board.map((rows, x) => {
-        return (
-          <div key={x}>
-            {rows.map((v, y) => (
-              <span keys={`${x}${y}`} onClick={(e) => handleClick(x, y)}>
-                {v}
-              </span>
-            ))}
-          </div>
-        );
-      })}
-    </>
-  );
-};
 
 function App() {
   var initialBoard = new Array(8);
@@ -46,8 +10,22 @@ function App() {
   initialBoard[4][4] = -1;
   initialBoard[3][4] = 1;
   initialBoard[4][3] = 1;
-  const [board, setBoardState] = useState(initialBoard);
+  const [board, setBoard] = useState(initialBoard);
   const [currentState, setCurrentState] = useState(-1); //黒
+  const [count, setCount] = useState(1);
+  const [countWhite, setCountWhite] = useState(0);
+
+  useEffect(() => {
+    var countWhiteLocal = 0;
+    if (count === 65) {
+      for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+          if (board[i][j] === 1) countWhiteLocal += 1;
+        }
+      }
+    }
+    setCountWhite(countWhiteLocal);
+  }, [count]);
 
   //置けるかどうかを判断する;
   // board[x][y] 置く位置
@@ -55,69 +33,117 @@ function App() {
   const isCanBePlaced = (x, y, state) => {
     //board[x][y]が0かどうかを判断する
     if (board[x][y] !== 0) return false;
-    //隣接に敵のコマがあるか
-    var flag = false;
-
+    console.log("start");
     for (var i = -1; i <= 1; i++) {
+      //boardの範囲外
+      if (x + i < 0 || x + i > 7) continue;
       for (var j = -1; j <= 1; j++) {
+        //隣接に敵のコマがあるか
+
+        //boardの範囲外
+        if (y + j < 0 || y + j > 7) continue;
+
         if (board[x + i][y + j] === state * -1) {
           //隣接に敵のコマがある場合、自分の色までループできるか
-          for (var k = 1; k <= 8; k++) {
-            if (board[x + i * k][x + j * k] === state) flag = true;
-            if (x + i * k > 8 || x + j * k > 8) break;
-            if (x + i * k < 0 || x + j * k < 0) break;
+          for (var k = 1; k <= 7; k++) {
+            //8方向に自分の駒があるかどうか
+            console.log("find");
+            console.log(x + i * k, y + j * k);
+
+            //boardの範囲外の場合は続き調べない
+            if (x + i * k > 7 || y + j * k > 7) break;
+            if (x + i * k < 0 || y + j * k < 0) break;
+
+            //空の場合は続き調べない
+            if (board[x + i * k][y + j * k] === 0) break;
+
+            //自分の駒がある場合、trueを返す
+            if (board[x + i * k][y + j * k] === state) return true;
           }
         }
       }
     }
-    //8方向に自分の駒があるかどうか
-    return flag;
+
+    return false;
   };
 
   //board配列の状態を更新する;
-  // location 置く位置
-  // state 置く色（1,-1）
-  // const updateBoard = (x, y, state) => {
-  //   //ひっくり返す処理
-  //   var flag = false;
-  //   for (var i = -1; i <= 1; i++) {
-  //     for (var j = -1; j <= 1; j++) {
-  //       if (board[x + i][y + j] == state * -1) {
-  //         //隣接に敵のコマがある場合、自分の色までループできるか
-  //         var reversePointX = -1;
-  //         var reversePointY = -1;
-  //         for (var k = 1; k <= 8; k++) {
-  //           if (board[x + i * k][y + j * k] == state) {
-  //             reversePointX = x + i * k;
-  //             reversePointY = y +j *k;
-  //             return [(reversePointX, reversePointY)];
-  //           }
-  //           if (x + i * k < 8 || x + j * k < 8) break;
-  //         }
-  //       }
-  //     }
-  //   }
+  //location 置く位置
+  //state 置く色（1,-1）
+  const updateBoard = (x, y, state, newBoard) => {
+    //ひっくり返す処理
+    for (var i = -1; i <= 1; i++) {
+      //boardの範囲外
+      if (x + i < 0 || x + i > 7) continue;
+      for (var j = -1; j <= 1; j++) {
+        //boardの範囲外
+        if (x + j < 0 || x + j > 7) continue;
 
-  //   if (flag) {
-  //   }
-  // };
+        if (board[x + i][y + j] === state * -1) {
+          //隣接に敵のコマがある場合、自分の色までループできるか
+
+          for (var k = 1; k <= 7; k++) {
+            //8方向に自分の駒があるかどうか
+
+            //boardの範囲外の場合は続き調べない
+            if (x + i * k > 7 || y + j * k > 7) break;
+            if (x + i * k < 0 || y + j * k < 0) break;
+            //空の場合は続き調べない
+            if (board[x + i * k][y + j * k] === 0) break;
+
+            //自分の駒がある場合、このターンで置いた駒からその駒までひっくり返す。
+            if (board[x + i * k][y + j * k] === state) {
+              // console.log("起点");
+              // console.log(x + i * k, y + j * k);
+              while (k > 1) {
+                //ひっくり返す。
+                k -= 1;
+                //console.log("ひっくり返す");
+                console.log(x + i * k, y + j * k);
+                newBoard[x + i * k][y + j * k] = state;
+              }
+
+              break;
+            }
+          }
+        }
+      }
+    }
+    setBoard(newBoard);
+  };
 
   const handleOnClick = (x, y) => {
     if (isCanBePlaced(x, y, currentState)) {
-      board[x][y] = currentState;
-      setBoardState((prevBoard) => {
-        const board = [...prevBoard];
-        board[x] = [...board[x]];
-        board[x][y] = currentState;
-        return board;
-      });
-      //updateBoard(x, y, currentState);
+      console.log("置ける");
+      const deepCopy = JSON.parse(JSON.stringify(board));
+      deepCopy[x][y] = currentState;
+      updateBoard(x, y, currentState, deepCopy);
+      setBoard(deepCopy);
       setCurrentState(currentState * -1);
+      setCount(count + 1);
     }
+  };
+
+  const resetBoard = () => {
+    setBoard(initialBoard);
+    setCurrentState(-1);
+    setCountWhite(0);
+    setCount(1);
   };
 
   return (
     <div className="App">
+      <h1>一人オセロ</h1>
+      {count < 65 ? (
+        <h2>
+          Turn {count}: {currentState === 1 ? "○" : "●"}
+        </h2>
+      ) : (
+        <h2>
+          ○ {countWhite} VS ● {64 - countWhite}
+        </h2>
+      )}
+
       <div>
         {board.map((rows, x) => {
           return (
@@ -126,7 +152,13 @@ function App() {
                 return (
                   <>
                     <span key={y} onClick={(e) => handleOnClick(x, y)}>
-                      {item}{" "}
+                      {item === 0 ? (
+                        <span style={{ color: "green" }}>□</span>
+                      ) : item === 1 ? (
+                        "○"
+                      ) : (
+                        "●"
+                      )}{" "}
                     </span>
                   </>
                 );
@@ -135,8 +167,10 @@ function App() {
           );
         })}
       </div>
+      <br />
+      <button onClick={resetBoard}>Reset</button>
     </div>
   );
 }
 
-export default App2;
+export default App;
